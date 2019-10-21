03a0aecf757a112c5731d8cdc0056d7eb92e5744
yadegariahmad <yadegariahmad@gmail.com>
2019-10-20 21:31:45 -0700

    Updates

---
+++ precache-manifest.00922c76c48e5935cf2f6402346f7a84.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f96e77cfc7959c521e32377d58c7d97",
+    "url": "/TicTacToe-Reactjs/index.html"
+  },
+  {
+    "revision": "fcba7f42948d73d109c9",
+    "url": "/TicTacToe-Reactjs/static/css/3.5ec480d0.chunk.css"
+  },
+  {
+    "revision": "1819b11d7ec6a6c67eef",
+    "url": "/TicTacToe-Reactjs/static/css/4.903f0280.chunk.css"
+  },
+  {
+    "revision": "e2775183e440e322afbb",
+    "url": "/TicTacToe-Reactjs/static/css/5.3a283d4d.chunk.css"
+  },
+  {
+    "revision": "ce9202aeaa27f8ab181f",
+    "url": "/TicTacToe-Reactjs/static/js/0.1aad49d6.chunk.js"
+  },
+  {
+    "revision": "fcba7f42948d73d109c9",
+    "url": "/TicTacToe-Reactjs/static/js/3.5eac56f1.chunk.js"
+  },
+  {
+    "revision": "1819b11d7ec6a6c67eef",
+    "url": "/TicTacToe-Reactjs/static/js/4.dba5b374.chunk.js"
+  },
+  {
+    "revision": "e2775183e440e322afbb",
+    "url": "/TicTacToe-Reactjs/static/js/5.9ba97792.chunk.js"
+  },
+  {
+    "revision": "0edfc52cbec73fd8594b",
+    "url": "/TicTacToe-Reactjs/static/js/main.79e7d3b9.chunk.js"
+  },
+  {
+    "revision": "641a97e62afac84dc43d",
+    "url": "/TicTacToe-Reactjs/static/js/runtime~main.8c891247.js"
+  },
+  {
+    "revision": "fd6b629efa80a18c29d114c93f737a25",
+    "url": "/TicTacToe-Reactjs/static/media/TicTacToe.fd6b629e.jpg"
+  }
+]);
